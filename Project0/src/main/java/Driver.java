7a0746d4b62b19ec3b72acd57804ea14d6375c09
jllilly <jlilly9892@gmail.com>
2021-07-15 18:21:14 -0700

    created classes and starting menu

---
+++ Project0/src/main/java/Driver.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; 
+public class Driver {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int menuSelection=0; 
+		
+		 
+
+		System.out.println("Hello! Please select an option"
+				+ "\n1. Register for an account"
+				+ "\n2. Login to account"
+				+ "\n3. Quit"); 
+		
+		menuSelection=in.nextInt();
+		
+	
+	
+	
+	
+	
+	
+	}
+	
+
+}
